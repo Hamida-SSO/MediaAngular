cd32434864608fb0cf26388fe8c7e347c9067d61
Hamida SIDI SAID OMAR <hsidisaidomar@DIGINAMIC.LOCAL>
2018-01-15 16:15:54 +0100

    update

---
+++ WebContent/App/js/mediaModule/filter/searchMedia.js
@@ -0,0 +1,17 @@
+angular.module('mediaModule').filter('searchMedia', function() {
+	return function(media, type1, type2, type3) {
+		console.log(type1,type2,type3);
+		var output = [];
+		for(var index in media) {
+			if(media[index].type == 'Livre' && type1){
+				output.push(media[index]);
+			} else if(media[index].type == 'CD' && type2){
+				output.push(media[index]);
+			} else if(media[index].type == 'DVD' && type3){
+				output.push(media[index]);
+			}
+		}
+		console.log(output);
+		return output;
+	}
+});
