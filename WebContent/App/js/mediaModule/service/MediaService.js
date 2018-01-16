4d505a43408005d32f42518942c715d3147efc94
Arnaud BONNET <abonnet@DIGINAMIC.LOCAL>
2018-01-16 16:42:10 +0100

    changed some buttons' color

--- WebContent/App/js/mediaModule/service/MediaService.js
+++ WebContent/App/js/mediaModule/service/MediaService.js
@@ -1,7 +1,7 @@
 angular.module('mediaModule').factory('MediaService', ['$http','$location', function($http,$location) {
 	
 
-	var promise1 = $http.get('http://192.168.1.14:8090/resource/media.recherche');
+	var promise1 = $http.get('http://192.168.1.15:8090/resource/media.recherche');
 	
 	var getAllBody = function() {
 		var promise2 = promise1.then(function(response) {
@@ -14,7 +14,7 @@ angular.module('mediaModule').factory('MediaService', ['$http','$location', func
 	var mediaPromises = {};
 	var getOneBody = function(id) {
 		if(!mediaPromises[id]) {
-			mediaPromises[id] = $http.get('http://192.168.1.14:8090/resource/media.accession',{params:{id:id}});
+			mediaPromises[id] = $http.get('http://192.168.1.15:8090/resource/media.accession',{params:{id:id}});
 		}
 		var promise2 = mediaPromises[id].then(function(response) {
 			console.log(response.data);
@@ -24,13 +24,13 @@ angular.module('mediaModule').factory('MediaService', ['$http','$location', func
 	};
 	
 	var updateBody = function(media) {
-		$http.post('http://192.168.1.14:8090/resource/media.modification', media).then(function(response) {
+		$http.post('http://192.168.1.15:8090/resource/media.modification', media).then(function(response) {
 			goMediaBody(response.data);
 		});
 	}
 	
 	var createBody = function(media) {
-		$http.post('http://192.168.1.14:8090/resource/media.creation', media).then(function(response) {
+		$http.post('http://192.168.1.15:8090/resource/media.creation', media).then(function(response) {
 		});
 	}
 	
